7806d8e7add9bea562cc404940e3acfc5e919713
sankalp-p <sam.fade@gmail.com>
2023-02-08 19:38:53 +0530

    Patient mobile app login page

---
+++ src/main/java/com/mobile/automation/MUtilities.java
@@ -0,0 +1,75 @@
+package com.mobile.automation;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class MUtilities  {
+
+	String projectlocation =null;
+
+	public String getProjectPath()
+	{
+		this.projectlocation = System.getProperty("user.dir");
+		return this.projectlocation;
+	}
+
+	public Properties readConfigFile()
+	{
+	
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+			input = new FileInputStream(this.getProjectPath()+"/src/main/java/com/web/configuration/config.properties");
+			prop.load(input);
+		}
+		catch(IOException ex)
+		{
+			System.out.println(ex.getMessage());
+		}finally {
+			if(input !=null)
+			{
+				try 
+				{
+					input.close();
+				}
+				catch(IOException e)
+				{
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+		return prop;
+	}
+	
+	public Properties readWebObjectRepoFile(String filename)
+	{
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+			input = new FileInputStream(this.getProjectPath()+"/src/main/java/com/web/configuration/config.properties");
+			prop.load(input);
+		}
+		catch(IOException ex)
+		{
+			System.out.println(ex.getMessage());
+		}finally {
+			if(input !=null)
+			{
+				try 
+				{
+					input.close();
+				}
+				catch(IOException e)
+				{
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+		return prop;
+	}
+}
+
